6698f4b039d6b5f82eb1d1cadbf8182b473bd032
dayhaysoos <ndejesus1227@gmail.com>
2020-04-04 22:31:57 -0400

    Updates

---
+++ precache-manifest.2955a32247441590e523f792c4ffe9a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ea8b4d3a67445dab6ca8d80d65294c",
+    "url": "/use-stripe-cart/index.html"
+  },
+  {
+    "revision": "e259c12afe53c3e93750",
+    "url": "/use-stripe-cart/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "e1ac1e57d6b6407a878d",
+    "url": "/use-stripe-cart/static/js/2.cd75e4c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/use-stripe-cart/static/js/2.cd75e4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e259c12afe53c3e93750",
+    "url": "/use-stripe-cart/static/js/main.3ec58c06.chunk.js"
+  },
+  {
+    "revision": "c1dea013724e0c13f964",
+    "url": "/use-stripe-cart/static/js/runtime-main.8356128b.js"
+  }
+]);
